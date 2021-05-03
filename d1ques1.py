692ac7a8873eef96fbe9abac4fa9f08723eb27d4
aman thakur <thakuraman2001@gmail.com>
2021-05-04 04:14:04 +0530

    d1ques1.py

---
+++ d1ques1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+for i in range(1,151):
+    if i%35==0:
+        print("TIPTOP")
+    elif i%5==0:
+        print("TIP")
+    elif i%7==0:
+        print("TOP")
+    else:
+        print(i)
+
+
+# In[ ]:
+
+
+
+
