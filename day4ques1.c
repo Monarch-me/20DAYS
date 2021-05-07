86ea80a428fe619d8f4470f373ac71cd64e44818
Monarch-me <82707975+Monarch-me@users.noreply.github.com>
2021-05-07 18:51:41 +0530

    Add files via upload

---
+++ day4ques1.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+int main() 
+{ 
+    int n; 
+    int arr [100];
+     int i;
+    int x = 0; // this is to store the continuous XOR  
+   printf("enter the size of array:\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf(" enter element #%d : ",i+1);
+		scanf("%d",&arr[i]);
+	}
+    for( i = 0;i<n;i++)
+	{ 
+        x = x ^ arr[i]; 
+    } 
+    printf("The non repeating element is : %d",x); 
+ 
+    return 0; 
+} 
