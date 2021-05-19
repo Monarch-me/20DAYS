6b2035b71f4fa61be8bcc7280af67e5cd52f6b55
Monarch-me <82707975+Monarch-me@users.noreply.github.com>
2021-05-19 21:16:55 +0530

    Add files via upload

---
+++ day16ques1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class day16ques1
+{
+    public static void reverse(char str[])
+    {
+        // Initialize left and right pointers
+        int r = str.length - 1, l = 0;
+
+        // Traverse string from both ends until
+        // 'l' and 'r'
+        while (l < r)
+        {
+            // Ignore special characters
+            if (!Character.isAlphabetic(str[l]))
+                l++;
+            else if(!Character.isAlphabetic(str[r]))
+                r--;
+
+                // Both str[l] and str[r] are not spacial
+            else
+            {
+                char tmp = str[l];
+                str[l] = str[r];
+                str[r] = tmp;
+                l++;
+                r--;
+            }
+        }
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+        System.out.println("enter string");
+        Scanner sc = new Scanner(System.in);
+        String str =sc.next();
+
+        char[] charArray = str.toCharArray();
+
+        System.out.println("Input string: " + str);
+        reverse(charArray);
+        String revStr = new String(charArray);
+
+        System.out.println("Output string: " + revStr);
+    }
+}
